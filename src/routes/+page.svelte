7e545dc0f6cab6bceb85c99996c6f39466af5c83
r3dnbl4ck <arbongeci@protonmail.ch>
2023-12-12 08:19:07 +0100

    tail

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../app.css";
+</script>
+
+<h2 class="text-2xl">smoqi</h2>
+
+<slot />
